f3e63b1c59af100546d1fa5105872ac09c0e8b93
LIMr1209 <aaa1058169464@126.com>
2019-03-12 17:11:57 +0800

    1.5.5.190312

--- classifier_compression/pruning_model.py
+++ classifier_compression/pruning_model.py
@@ -1,5 +1,9 @@
 import csv
+from ruamel import yaml
 
+with open('resnet152.schedule_sensitivity.yaml', 'r', encoding='utf-8') as f:
+    data = yaml.load(f, Loader=yaml.RoundTripLoader)
+# 模型敏感性分析后创建剪枝计划表  yaml
 pruning_dict = {}
 
 with open('sensitivity.csv', newline='') as csvfile:
@@ -12,7 +16,15 @@ with open('sensitivity.csv', newline='') as csvfile:
             pruning_dict[low[0]] = [{low[1]: low[2]}]
         else:
             pruning_dict[low[0]].append({low[1]: low[2]})
+
+sensitivities_dict = {}
 for key, value in pruning_dict.items():
     for i in range(len(value)):
         if float(list(value[i].values())[0]) < 90:
-            print("'" + key + "': " + list(value[i - 1].keys())[0])
+            sensitivities_dict[key] = round(float(list(value[i - 1].keys())[0]), 2)
+            break
+# 修改计划
+data['pruners']['pruner1']['sensitivities'] = sensitivities_dict
+# 保存写入文件
+with open('resnet152.schedule_sensitivity.yaml', "w", encoding="utf-8") as f:
+    yaml.dump(data, f, Dumper=yaml.RoundTripDumper)
