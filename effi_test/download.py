fda4d40e73d33b189a1928723f9ff7bd17805938
LIMr1209 <aaa1058169464@126.com>
2019-06-06 19:32:08 +0800

    EfficientNet

---
+++ effi_test/download.py
@@ -0,0 +1,5 @@
+from efficientnet_pytorch import EfficientNet
+model = EfficientNet.from_pretrained('efficientnet-b3')
+model = EfficientNet.from_pretrained('efficientnet-b2')
+model = EfficientNet.from_pretrained('efficientnet-b1')
+model = EfficientNet.from_pretrained('efficientnet-b0')
